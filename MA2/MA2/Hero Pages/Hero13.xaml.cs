ba57642e0cf8a390f48b1c1e12454fb6d0c520f4
Stephen Murphy <Stephen Murphy>
2017-03-14 21:17:35 +0000

    hero pages updated

--- MA2/MA2/Hero Pages/Hero13.xaml.cs
+++ MA2/MA2/Hero Pages/Hero13.xaml.cs
@@ -26,5 +26,10 @@ namespace MA2.Hero_Pages
         {
             this.InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Heroes), UriKind.Relative);
+        }
     }
 }
